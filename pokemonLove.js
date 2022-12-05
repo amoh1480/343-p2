bf467e48a5f4983ac56b80e003b3b4aee709b239
amoh1480 <amohammadali1480@gmail.com>
2022-12-05 13:30:43 -0500

    Abdoodoos girlfirend is real

--- pokemonLove.js
+++ pokemonLove.js
@@ -134,3 +134,27 @@ fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0').then(response =
     });
 });
 
+
+
+
+function filterSearch() {
+      // Declare variables
+      var input, filter, ul, li, a, i, txtValue;
+      input = document.getElementById('myInput');
+      filter = input.value.toUpperCase();
+      ul = document.getElementById("myUL");
+      li = ul.getElementsByTagName('li');
+    
+      // Loop through all list items, and hide those who don't match the search query
+      for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          li[i].style.display = "";
+        } else {
+          li[i].style.display = "none";
+        }
+      }
+    }
+
+    // <li><a href="#"></a></li>
