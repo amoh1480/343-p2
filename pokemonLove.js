7a41c477eec0072ccd35fe017732ad764b3a3e18
Alyssa Sharp <alykatherineee@gmail.com>
2022-12-05 11:53:48 -0500

    weeded out non-default, changed to offical artwork

--- pokemonLove.js
+++ pokemonLove.js
@@ -9,14 +9,44 @@ const options = {
 let person1 = document.getElementById('person1');
 let person2 = document.getElementById('person2');
 
+function getValidPokemon() {
+    // console.log("in get valid");
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0').then(response => {
+        response.json().then(data => {
+            let allPokemon = data.results;
+            let pokemon = allPokemon[Math.floor(Math.random() * allPokemon.length)];
+            // console.log("here is a pokemon");
+            // console.log(pokemon);
+            fetch(pokemon.url).then(response => {
+                response.json().then(pokemon_stats => {
+                    // console.log("here are the stats");
+                    // console.log(pokemon.name);
+                    // console.log(pokemon_stats);
+                    // console.log(pokemon_stats.species);
+                    if(pokemon_stats.name != pokemon_stats.species.name) {
+                        return getValidPokemon();
+                    } else {
+                        // console.log("i have reached the result");
+                        // console.log(pokemon);
+                        return pokemon;
+                    }
+                })
+            });
+        });
+    });
+}
+
 // pokemon API
 // https://pokeapi.co/api/v2/pokemon/ditto
 
 // all pokemon ?
 // https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0
 
+// all pokemon species
+// https://pokeapi.co/api/v2/pokemon-species?limit=100000&offset=0
+
 // get all pokemon
-fetch('https://pokeapi.co/api/v2/pokemon-species?limit=100000&offset=0').then(response => {
+fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0').then(response => {
     response.json().then(data => {
         // all pokemon
         let pokemon = data.results;
@@ -35,8 +65,29 @@ fetch('https://pokeapi.co/api/v2/pokemon-species?limit=100000&offset=0').then(re
         // random pokemon
         // TODO: combo box of all pokemon         
         let pokemon1 = pokemon[Math.floor(Math.random() * pokemon.length)];
+        fetch(pokemon1.url).then(response => {
+            response.json().then(pokemon_stats => {
+                for (let i = 0; i < 20; i++) {
+                    if (pokemon_stats.name == pokemon_stats.species.name) {
+                        break;
+                    }
+                    pokemon1 = pokemon[Math.floor(Math.random() * pokemon.length)];
+                }
+            });
+        });
         console.log(pokemon1);
+
         let pokemon2 = pokemon[Math.floor(Math.random() * pokemon.length)];
+        fetch(pokemon2.url).then(response => {
+            response.json().then(pokemon_stats => {
+                for (let i = 0; i < 20; i++) {
+                    if (pokemon_stats.name == pokemon_stats.species.name) {
+                        break;
+                    }
+                    pokemon2 = pokemon[Math.floor(Math.random() * pokemon.length)];
+                }
+            });
+        });
         console.log(pokemon2);
 
         // console.log(pokemon1.name);
@@ -51,20 +102,24 @@ fetch('https://pokeapi.co/api/v2/pokemon-species?limit=100000&offset=0').then(re
                     person1.appendChild(person1Name);
                     fetch(pokemon1.url).then(response => {
                         response.json().then(data => {
+                            console.log(data.sprites.other);
                             let person1Image = document.createElement('img');
-                            person1Image.src = data.sprites.front_default;
+                            // person1Image.src = data.sprites.front_default;
+                            person1Image.src = data.sprites.other["official-artwork"].front_default;
                             person1.appendChild(person1Image);
                         })
                     });
 
                     // person 2
+                    console.log(pokemon2);
                     let person2Name = document.createElement('h2');
                     person2Name.innerText = pokemon2.name;
                     person2.appendChild(person2Name);
                     fetch(pokemon2.url).then(response => {
                         response.json().then(data => {
                             let person2Image = document.createElement('img');
-                            person2Image.src = data.sprites.front_default;
+                            // person2Image.src = data.sprites.front_default;
+                            person2Image.src = data.sprites.other["official-artwork"].front_default;
                             person2.appendChild(person2Image);
                         })
                     });
